b57bf1487f74f226a261fa6538551166e2360973
Igor X <iverbitskiy321@gmail.com>
2023-06-13 12:26:00 +0300

    remove debug in project

--- WebGallery/Assets/Game/Code/ServiceModule/ContentPositionRememberer.cs
+++ WebGallery/Assets/Game/Code/ServiceModule/ContentPositionRememberer.cs
@@ -17,8 +17,6 @@ namespace WebGallery.ServiceModule
 		private static void SetContentPosition(ScrollRect __scrollRect)
 		{
 			__scrollRect.normalizedPosition = new Vector2(__scrollRect.normalizedPosition.x, PlayerYPosOnContent);
-			Debug.Log(PlayerYPosOnContent);
-			Debug.Log(__scrollRect.verticalNormalizedPosition);
 		}
 	}
 }
